81262cebc564eadd732bf2c6715a05a402c52b6d
Harsh Jayswal <harshjayswal65@gmail.com>
2026-08-19 18:27:19 +0530

    Time: 51 ms (5.06%), Space: 52.7 MB (5.04%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool isSafe(int n, vector<string>board,int row , int col){
+        if(row < 0 || row >= n || col < 0 || col >= n) return false;
+        for(int i = 0;i<n;i++){
+            if(board[row][i] == 'Q') return false;
+        }
+        for(int i = 0;i<n;i++){
+            if(board[i][col] == 'Q') return false;
+        }
+
+        for(int i = row,j= col ; i>=0 && j>=0 ; i--,j--){
+            if(board[i][j] == 'Q') return false;
+        }
+        for(int i = row,j= col ; i>=0 && j<n ; i--,j++){
+            if(board[i][j] == 'Q') return false;
+        }
+        return true;
+    }
+    int solver(int n, vector<string>board,int row ){
+        if(row == n){
+            return 1;
+        }
+        int count = 0;
+        for(int j=0; j<n; j++){
+            if(isSafe(n,board,row,j)){
+                board[row][j] = 'Q';
+                count += solver(n,board,row+1);
+                board[row][j] = '.';
+            }
+        }
+        return count;
+    }
+    int totalNQueens(int n) {
+        vector<string>board(n,string(n,'.'));
+        return solver(n,board,0);
+    }
+};
