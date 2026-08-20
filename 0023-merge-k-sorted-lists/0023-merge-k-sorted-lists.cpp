c0c291290219423612a46591b1b6fce8e7d67487
Harsh Jayswal <harshjayswal65@gmail.com>
2026-08-20 17:47:50 +0530

    Time: 0 ms (100%), Space: 18.5 MB (48.6%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -10,33 +10,35 @@
  */
 class Solution {
 public:
-
     ListNode* merge(ListNode* left,ListNode* right){
-        ListNode* bummy = new ListNode(-1);
-        ListNode* temp = bummy;
-        while(left && right){
-            if(left->val <= right->val){
-                temp->next = left;
-                left=left->next;
-            }else{
-                temp->next = right;
-                right=right->next;
-            }
-            temp  = temp->next;
-        }
-        if(left){
+    ListNode* bummy = new ListNode(-1);
+    ListNode* temp = bummy;
+    while(left && right){
+        if(left->val <= right->val){
             temp->next = left;
+            left=left->next;
         }else{
             temp->next = right;
+            right=right->next;
         }
-        return bummy->next;
+        temp  = temp->next;
+    }
+    if(left){
+        temp->next = left;
+    }else{
+        temp->next = right;
+    }
+    return bummy->next;
     }
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         if(lists.empty()) return nullptr;
-        ListNode* temp = lists[0];
-        for(int j =1;j<lists.size();j++){
-            temp = merge(temp,lists[j]);
-        }
-        return temp;
+        int interval = 1;
+        while(interval<lists.size()){
+            for(int i=0;i+interval<lists.size();i+=interval*2){
+                lists[i] = merge(lists[i],lists[i+interval]);
+            }
+            interval *=2;
+        } 
+        return lists[0];
     }
 };
