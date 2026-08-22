286d037dfe1704a77e021367cf8ed8280695594d
Harsh Jayswal <harshjayswal65@gmail.com>
2026-08-22 22:49:05 +0530

    Time: 20 ms (62.68%), Space: 24 MB (90.96%) - LeetHub

--- 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.cpp
+++ 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
-    int pivit(vector<int>& nums, int st, int end){
-        int piv = nums[end];
-        int j=st-1;
-        for(int i=st; i<end ;i++){
-            if(nums[i]<piv){
-                j++;
-                swap(nums[i],nums[j]);
+    int maxProductDifference(vector<int>& nums) {
+        int largest = 0, secondLargest = 0;
+        int smallest = INT_MAX, secondSmallest = INT_MAX;
+
+        for (int n : nums) {
+            if (n < smallest) {
+                secondSmallest = smallest;
+                smallest = n;
+            } else if (n < secondSmallest) {
+                secondSmallest = n;
+            }
+
+            if (n > largest) {
+                secondLargest = largest;
+                largest = n;
+            } else if (n > secondLargest) {
+                secondLargest = n;
             }
         }
-        j++;
-        swap(nums[end], nums[j]);
-        return j;
-    }
-    void QuickSort(vector<int>& nums, int st, int end){
-        if(st>=end) return;
-        int p = pivit(nums,st,end);
-        QuickSort(nums, st,p-1);
-        QuickSort(nums ,p+1 , end);
-    }
-    int maxProductDifference(vector<int>& nums) {
-        int n = nums.size()-1;
-        QuickSort(nums, 0, n);
-        int pro1 = nums[0]*nums[1];
-        int pro2 = nums[n]*nums[n-1];
-        return pro2-pro1;
+
+        return (largest * secondLargest) - (smallest * secondSmallest);
     }
 };
